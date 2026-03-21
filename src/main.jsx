1fdd28d035a18470f814010bbc9059bef49f3663
Ifeoluwa <adelekanifeoluwa1402@gmail.com>
2026-03-20 23:49:15 -0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -3,16 +3,6 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
-document.querySelectorAll('a[href^="#"]').forEach((anchor) => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault()
-    const target = document.querySelector(this.getAttribute('href'))
-    if (target) {
-      target.scrollIntoView({ behavior: 'smooth' })
-    }
-  })
-})
-
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
